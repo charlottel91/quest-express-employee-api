bab25cf1bda00e9d168f295e741a8c7467b2b8fc
Charlotte LECONTE <charlotte.leconte91@gmail.com>
2020-02-04 10:57:52 +0100

    routes to have employees

--- conf.js
+++ conf.js
@@ -4,8 +4,8 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: 'localhost', // db server address
   user: 'root', // db user's name
-  password: '', // db user's password
-  database: 'company', // db name
+  password: 'Yolo6789', // db user's password
+  database: 'movies', // db name
 });
 
 module.exports = connection;
--- index.js
+++ index.js
@@ -2,21 +2,44 @@ const express = require('express');
 const connection = require('./conf');
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 // respond to requests on `/api/employees`
-app.get('/api/employees', (req, res) => {
-  // send an SQL query to get all employees
-  connection.query('SELECT * FROM employee', (err, results) => {
-    if (err) {
-      // If an error has occurred, then the client is informed of the error
-      res.status(500).send(`An error occurred: ${err.message}`);
-    } else {
-      // If everything went well, we send the result of the SQL query as JSON
-      res.json(results);
-    }
-  });
-});
+// app.get('/api/employees', (req, res) => {
+//   let sql = 'SELECT * FROM employee';
+//   const sqlValues = [];
+//   if (req.query.department) {
+//     sql += ' WHERE department = ?';
+//     sqlValues.push(req.query.department);
+//   }
+//   // send an SQL query to get all employees
+//   connection.query(sql, sqlValues, (err, results) => {
+//     if (err) {
+//       // If an error has occurred, then the client is informed of the error
+//       res.status(500).send(`An error occurred: ${err.message}`);
+//     } else {
+//       // If everything went well, we send the result of the SQL query as JSON
+//       res.json(results);
+//     }
+//   });
+// });
+
+// // respond to requests on `/api/employees/:id`
+// app.get('/api/employees/:id', (req, res) => {
+//   // send an SQL query to get all employees
+//   connection.query('SELECT * FROM employee WHERE id=?', req.params.id, (err, results) => {
+//     if (err) {
+//       // If an error has occurred, then the client is informed of the error
+//       res.status(500).send(`An error occurred: ${err.message}`);
+//     }
+//     if (results.length === 0) {
+//       res.status(404).send('Employee not found')
+//     } else {
+//       // If everything went well, we send the result of the SQL query as JSON
+//       res.json(results);
+//     }
+//   });
+// });
 
 app.listen(port, (err) => {
   if (err) {
